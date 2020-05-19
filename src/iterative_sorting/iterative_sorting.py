258bf1423e10e36eae4d0bc4e37f2ee3d58bff08
qb1968 <you@example.com>
2020-05-19 15:31:35 -0400

    Final

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,31 +7,56 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-    for j in range(cur_index, len(arr)):
-            if arr[j] < arr[smallest_index]:
-                smallest_index = j 
+    for smallest_index in range(i + 1, len(arr)):
+            if arr[smallest_index] < arr[cur_index]:
+                cur_index = smallest_index
 
         # TO-DO: swap
         # Your code here
-    arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+    arr[i], arr[cur_index] = arr[cur_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-    for each in range(0, len(arr)-1):
-        for item in range(0, len(arr) - each - 1):
-            if arr[item] > arr[item + 1]:
-                arr[item], arr[item + 1] = arr[item + 1], arr[item]
+    length = len(arr)
+    # loop through all array elements
+    for i in range(length - 1):
+        #  loop through array from 0 to length -1
+        for j in range(0, length - 1 - i):
+            # swap elements if greater than next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-# def count_sort(arr, maximum=-1):
-#     # Your code here
-#     pass
 
-#     return arr
+def count_sort(arr, maximum=-1):
+    if len(arr) == 0:
+        return arr
+
+    if maximum == -1:
+        maximum = max(arr)
+
+    buckets = [0 for _ in range(maximum + 1)]
+
+    for x in arr:
+        if x < 0:
+            return 'Error, negative numbers not allowed in Count Sort'
+        buckets[x] += 1
+
+    j = 0
+    for i in range(len(buckets)):
+        while buckets[i] > 0:
+            arr[j] = i
+            j += 1
+            buckets[i] -= 1
+    return arr
+   
+ #     pass
+
+     
